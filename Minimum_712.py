c5e81ba49285a2c2f9b91fd7d0564a3514d420b4
YuLi <wstcliyu.gmail.com>
2020-03-07 00:37:30 -0500

    712. Minimum ASCII Delete Sum for Two Strings

--- Minimum_712.py
+++ Minimum_712.py
@@ -1,4 +1,4 @@
-class Solution:
+class Minimum_712:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         m, n = len(s1), len(s2)
         dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
