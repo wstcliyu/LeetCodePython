00934972e61b07b2f70b6d05e3b7b2039cddc010
YuLi <wstcliyu.gmail.com>
2020-03-08 20:03:44 -0400

    Practice Parameters

---
+++ practice_parameters.py
@@ -0,0 +1,94 @@
+
+# https://www.liaoxuefeng.com/wiki/1016959663602400/1017261630425888
+
+# 参数顺序：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+
+
+
+
+# 定义默认参数要牢记一点：默认参数必须指向不变对象！
+# 为什么要设计str、None这样的不变对象呢？因为不变对象一旦创建，对象内部的数据就不能修改，这样就减少了由于修改数据导致的错误。
+# 此外，由于对象不变，多任务环境下同时读取对象不需要加锁，同时读一点问题都没有。我们在编写程序时，如果可以设计一个不变对象，那就尽量设计成不变对象。
+
+# 错误示范
+def add_end(L=[]):
+    L.append('END')
+    return L
+
+# 正确示范
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+
+
+
+
+# 可变参数
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+calc(1, 2, 3)
+nums = [1, 2, 3]
+calc(*nums)
+# *nums表示把nums这个list的所有元素作为可变参数传进去。这种写法相当有用，而且很常见。
+
+
+
+
+# 关键字参数
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+
+person('Bob', 35, city='Beijing')
+person('Adam', 45, gender='M', job='Engineer')
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+person('Jack', 24, **extra)
+# **extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，
+# 注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra。
+
+
+
+
+# 命名关键字参数
+# 对于关键字参数，函数的调用者可以传入任意不受限制的关键字参数。至于到底传入了哪些，就需要在函数内部通过kw检查。
+def person(name, age, **kw):
+    if 'city' in kw:
+        pass
+    if 'job' in kw:
+        pass
+    print('name:', name, 'age:', age, 'other:', kw)
+
+# 但是调用者仍可以传入不受限制的关键字参数：
+person('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
+
+
+# 如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下：
+def person(name, age, *, city, job):
+    print(name, age, city, job)
+# 和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+person('Jack', 24, city='Beijing', job='Engineer')
+
+
+# 如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了：
+def person(name, age, *args, city, job):
+    print(name, age, args, city, job)
+# 命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错：
+person('Jack', 24, 'Beijing', 'Engineer')
+# 由于调用时缺少参数名city和job，Python解释器把这4个参数均视为位置参数，但person()函数仅接受2个位置参数。
+
+
+# 命名关键字参数可以有缺省值，从而简化调用：
+def person(name, age, *, city='Beijing', job):
+    print(name, age, city, job)
+# 由于命名关键字参数city具有默认值，调用时，可不传入city参数：
+person('Jack', 24, job='Engineer')
