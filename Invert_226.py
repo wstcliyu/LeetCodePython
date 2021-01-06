38de0aef71d52eff797bfdced24cfcac0a5bb0ee
Yu Li <38941676+wstcliyu@users.noreply.github.com>
2021-01-06 00:16:27 -0800

    226. Invert Binary Tree

--- Invert_226.py
+++ Invert_226.py
@@ -1,5 +1,26 @@
 # from collections import deque
 
+"""
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+
 class Invert_226:
     """ Recursive
     def invertTree(self, root: TreeNode) -> TreeNode:
