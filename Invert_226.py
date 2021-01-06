f34078aea32c280e33944e7685d5d767113ee203
Yu Li <38941676+wstcliyu@users.noreply.github.com>
2021-01-06 00:14:32 -0800

    226. Invert Binary Tree

---
+++ Invert_226.py
@@ -0,0 +1,59 @@
+# from collections import deque
+
+class Invert_226:
+    """ Recursive
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+    """
+    
+    
+    
+    """ Iterative with queue (level order)
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        q = collections.deque([root])
+        while q:
+            node = q.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return root
+    """
+    
+    
+    
+    """ Iterative with stack #1 (pre order)
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack += node.left, node.right
+        return root
+    """
+    
+    
+    
+    """ Iterative with stack #2 (pre order)
+    """
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        stack = []
+        p = root
+        while stack or p:
+            if p:
+                stack.append(p)
+                p.left, p.right = p.right, p.left
+                p = p.left
+            else:
+                p = stack.pop().right
+        return root
