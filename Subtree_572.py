583399cb28b3a20a6f26e485a6fdc27d187ae2d7
Yu Li <38941676+wstcliyu@users.noreply.github.com>
2021-01-06 23:55:22 -0800

    572. Subtree of Another Tree

---
+++ Subtree_572.py
@@ -0,0 +1,46 @@
+"""
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+ 
+
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false.
+"""
+
+class Subtree_572:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        def equals(s: TreeNode, t: TreeNode) -> bool:
+            if not s and not t:
+                return True
+            if not s or not t:
+                return False
+            return s.val == t.val and equals(s.left, t.left) and equals(s.right, t.right)
+        if not s:
+            return equals(s, t)
+        return equals(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
