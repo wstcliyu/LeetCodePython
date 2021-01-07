33388037ab2a92dfcb24b0798d668ce4c21673f6
Yu Li <38941676+wstcliyu@users.noreply.github.com>
2021-01-06 22:37:01 -0800

    560. Subarray Sum Equals K

--- Subarray_560.py
+++ Subarray_560.py
@@ -1,3 +1,16 @@
+"""
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+"""
+
 class Subarray_560:
     def subarraySum(self, nums: List[int], k: int) -> int:
         res, sum_so_far = 0, 0
